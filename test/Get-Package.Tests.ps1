05730620f6140a92cf988156c03c4a0ef1b7ef39
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-03-29 13:43:43 -0500

    Format tests file

--- test/Get-Package.Tests.ps1
+++ test/Get-Package.Tests.ps1
@@ -4,19 +4,19 @@ Describe Get-Package {
     Context 'with no parameters' {
         It 'should return results' {
             Get-Package |
-            Should -Not -BeNullOrEmpty
+                Should -Not -BeNullOrEmpty
         }
     }
 
     Context 'with -Name parameter' {
         It 'should return Microsoft.WindowsFeedbackHub' {
             Get-Package -Name Microsoft.WindowsFeedbackHub |
-            Should -Not -BeNullOrEmpty
+                Should -Not -BeNullOrEmpty
         }
 
         It 'should return with wildcards' {
             Get-Package -Name Microsoft* |
-            Should -Not -BeNullOrEmpty
+                Should -Not -BeNullOrEmpty
         }
     }
 }
